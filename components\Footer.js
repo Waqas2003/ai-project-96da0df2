7b16b69cd59a35583912704f7ca7b217b9ae50c4
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-11 17:55:04 +0500

    Add components\Footer.js

---
+++ components\Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="bg-gray-200 py-12">
+      <div className="container mx-auto p-4 pt-6 md:p-6 lg:p-12">
+        <p className="text-gray-600">Copyright 2023 Pakistan Tourism. All rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
